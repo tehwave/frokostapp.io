6cf27318441f8d655a220604e7864b937be63555
Peter Jørgensen <peterchrjoergensen@gmail.com>
2019-11-03 21:40:32 +0100

    adjust padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                             <div class="card-header bg-pattern p-4">
                                 {{--  --}}
                             </div>
-                            <div class="card-body px-6">
+                            <div class="card-body p-6">
                                 <h1 class="display-3 text-danger">FROKOST</h1>
                                 <p class="lead">Choose who's gonna get lunch with an impartial robot.</p>
                                 <a href="{{ route('slack.redirect') }}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
