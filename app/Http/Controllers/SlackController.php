9073ca3cd8b461730595068cfe52dc684bb150d0
Peter Jørgensen <peterchrjoergensen@gmail.com>
2020-02-17 19:37:59 +0100

    why was this necessary

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -2,45 +2,255 @@
 
 namespace App\Http\Controllers;
 
+use Str;
 use Socialite;
 use App\Slack;
 use Illuminate\Http\Request;
+use InvalidArgumentException;
+use Illuminate\Http\RedirectResponse;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 
 class SlackController extends Controller
 {
     /**
+     * The type of the encoding in the query.
+     *
+     * @var int Can be either PHP_QUERY_RFC3986 or PHP_QUERY_RFC1738.
+     */
+    protected $encodingType = PHP_QUERY_RFC1738;
+
+    /**
+     * The custom parameters to be sent with the request.
+     *
+     * @var array
+     */
+    protected $parameters = [];
+
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = [
+        'chat:write',
+        'users:read',
+    ];
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ',';
+
+    /**
+     * The custom Guzzle configuration options.
+     *
+     * @var array
+     */
+    protected $guzzle = [];
+
+    /**
+     * The HTTP Client instance.
+     *
+     * @var \GuzzleHttp\Client
+     */
+    protected $httpClient;
+
+    /**
      * Redirect to authorize.
      *
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function redirect()
+    public function redirect(Request $request)
     {
-        return Socialite::with('slack')
-            ->scopes(['users:read', 'chat:write:bot'])
-            ->redirect();
+        $state = Str::random(40);
+
+        $request->session()->put('state', $state);
+
+        return new RedirectResponse($this->getAuthUrl($state));
+    }
+
+    /**
+     * Get the authentication URL for the provider.
+     *
+     * @param  string  $state
+     * @return string
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://slack.com/oauth/v2/authorize', $state
+        );
+    }
+
+    /**
+     * Get the authentication URL for the provider.
+     *
+     * @param  string  $url
+     * @param  string  $state
+     * @return string
+     */
+    protected function buildAuthUrlFromBase($url, $state)
+    {
+        return sprintf('%s?%s', $url, http_build_query(
+            $this->getCodeFields($state),
+            '',
+            '&',
+            $this->encodingType
+        ));
+    }
+
+    /**
+     * Get the GET parameters for the code request.
+     *
+     * @param  string|null  $state
+     * @return array
+     */
+    protected function getCodeFields($state)
+    {
+        $fields = [
+            'client_id' => config('services.slack.client_id'),
+            'redirect_uri' => config('services.slack.redirect'),
+            'scope' => $this->formatScopes($this->getScopes(), $this->scopeSeparator),
+            'response_type' => 'code',
+            'state' => $state,
+        ];
+
+        return array_merge($fields, $this->parameters);
+    }
+
+    /**
+     * Format the given scopes.
+     *
+     * @param  array  $scopes
+     * @param  string  $scopeSeparator
+     * @return string
+     */
+    protected function formatScopes(array $scopes, $scopeSeparator)
+    {
+        return implode($scopeSeparator, $scopes);
+    }
+
+    /**
+     * Get the current scopes.
+     *
+     * @return array
+     */
+    public function getScopes()
+    {
+        return $this->scopes;
     }
 
     /**
      * Create Slack, and redirect to the settings.
      *
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function callback()
+    public function callback(Request $request)
     {
-        $result = Socialite::driver('slack')
-            ->user()
-            ->accessTokenResponseBody;
+        if ($this->hasInvalidState($request)) {
+            throw new InvalidArgumentException;
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode($request));
+
+        dd($response);
 
         $slack = Slack::updateOrCreate([
             'team_id'       => $result['team_id'],
         ], [
             'access_token'  => $result['access_token'],
-            'team_name'     => $result['team_name'],
+            'team_name'     => $result['team']['name'],
             'data'          => $result,
         ]);
 
         return redirect()->route('slack.dashboard', [
             'slack' => $slack->uuid,
         ]);
+    }
+
+    /**
+     * Determine if the current request / session has a mismatching "state".
+     *
+     * @return bool
+     */
+    protected function hasInvalidState(Request $request)
+    {
+        $state = $request->session()->pull('state');
+
+        return ! (strlen($state) > 0 && $request->input('state') === $state);
+    }
+
+    /**
+     * Get the access token response for the given code.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    public function getAccessTokenResponse($code)
+    {
+        $postKey = (version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
+
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Accept' => 'application/json'],
+            $postKey => $this->getTokenFields($code),
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Get a instance of the Guzzle HTTP client.
+     *
+     * @return \GuzzleHttp\Client
+     */
+    protected function getHttpClient()
+    {
+        if (is_null($this->httpClient)) {
+            $this->httpClient = new Client($this->guzzle);
+        }
+
+        return $this->httpClient;
+    }
+
+    /**
+     * Get the token URL for the provider.
+     *
+     * @return string
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://slack.com/api/oauth.v2.access';
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return [
+            'client_id' => config('services.slack.client_id'),
+            'client_secret' => config('services.slack.client_secret'),
+            'code' => $code,
+            'redirect_uri' => config('services.slack.redirect'),
+        ];
+    }
+
+    /**
+     * Get the code from the request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return string
+     */
+    protected function getCode(Request $request)
+    {
+        return $request->input('code');
     }
 }
